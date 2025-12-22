86f4a0950abc27713fec8031fe7d74f922010497
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-12-22 17:52:34 +0100

    initialize micro-services project

---
+++ src/main/java/ma/sieger/keynoteconnect/KeynoteConnectApplication.java
@@ -0,0 +1,13 @@
+package ma.sieger.keynoteconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeynoteConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeynoteConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/sieger/keynoteconnect/KeynoteConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sieger.keynoteconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeynoteConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
