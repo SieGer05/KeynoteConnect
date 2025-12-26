d7253c5fb81b896fd92ed90c99d6c92767c36355
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-12-26 21:33:19 +0100

    add Footer component and update App layout for improved structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-slate-50">
+      <div className="min-h-screen bg-slate-50 flex flex-col">
         <Navbar />
         <main>
           <AppRouter />
         </main>
+
+        <Footer />
       </div>
     </BrowserRouter>
   );
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+   return (
+      <footer className="bg-white border-t border-slate-200 py-6 mt-auto">
+         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+         <p className="text-center text-slate-500 text-sm font-medium">
+            &copy; {new Date().getFullYear()} - Copyright by: **DJILI Mohamed Amine**
+         </p>
+         </div>
+      </footer>
+   );
+};
+
+export default Footer;
